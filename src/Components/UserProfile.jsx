3c25567026a8c33e2327a693b120cd5e4a5cdbd7
LeoAlexThomas <leoalex960@gmail.com>
2025-12-24 11:34:53 +0530

    moved userprofile component to seprate file

---
+++ src/Components/UserProfile.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserInfoContext } from "../Context/UserInfoContext";
+import { getUserRoleLabel, removeToken } from "../Utils/common";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { MdPerson } from "react-icons/md";
+
+const UserProfile = () => {
+  const navigate = useNavigate();
+  const { user } = useUserInfoContext();
+  const handleLogoutClick = () => {
+    removeToken();
+    navigate("/login");
+  };
+
+  return (
+    <Menu>
+      <MenuButton className="w-full max-w-40">
+        <div className="flex gap-2 items-center p-2 rounded-lg bg-neutral w-full">
+          <MdPerson className="w-8 h-8" />
+          <div className="flex flex-col gap-1">
+            <p className="text-base font-semibold text-start">{user?.name}</p>
+            <p className="text-sm text-secondary-hover font-bold">
+              {getUserRoleLabel(user?.role)}
+            </p>
+          </div>
+        </div>
+      </MenuButton>
+      <MenuItems
+        anchor="bottom"
+        transition
+        className="p-2 w-48 bg-neutral rounded-lg shadow-lg mt-1 origin-top transition duration-200 ease-out data-closed:scale-95 data-closed:opacity-0"
+      >
+        <MenuItem as="a" href="/profile">
+          My Profile
+        </MenuItem>
+        <MenuItem as="button" onClick={handleLogoutClick}>
+          LOGOUT
+        </MenuItem>
+      </MenuItems>
+    </Menu>
+  );
+};
+
+export default UserProfile;
